b2ebaf69bc108cc36c71eac63e91db73a8e75a0d
BlueGlassBlock <blueglassblock@outlook.com>
2025-04-30 01:28:41 +0800

    chore: simplify dependency

---
+++ src/_cookbook/mod.rs
@@ -0,0 +1,4 @@
+
+//! # Cookbook for `cmdpal-rs`
+//! 
+//! Welcome to the `cmdpal-rs` cookbook! This module serves as a guidance for developers looking to start developing their Command Palette extensions with `cmdpal-rs` as well as references for internals of the Command Palette extension interface.
