03680a95808afb39f7ff38b26da5762e3a52b95c
Muhammad Ragib Hasin <ragib.hasin@gmail.com>
2026-08-25 19:22:50 +0600

    Cleanup systest example

--- systest/src/main.rs
+++ systest/src/main.rs
@@ -1,6 +1,4 @@
-use cmdpal::prelude::WinResult as Result;
 use cmdpal::prelude::*;
-use windows::Win32;
 
 //255c6090-dbec-4008-b865-3f08765e727b
 //0x255c6090_dbec_4008_b865_3f08765e727b
@@ -35,28 +33,27 @@ fn com_main() -> Result<()> {
     );
     let model = settings.add_setting(
         ChoiceSetSetting::<&str>::new("llm-model")
-            .add_choice("gpt-3.5-turbo".into())
-            .add_choice("gpt-4".into())
-            .add_choice("gpt-4o".into())
-            .default("gpt-3.5-turbo".into())
+            .add_choice("gpt-3.5-turbo")
+            .add_choice("gpt-4")
+            .add_choice("gpt-4o")
+            .default("gpt-3.5-turbo")
             .caption("Model"),
     );
 
-    let md_box: ComObject<_> = cmdpal::content::markdown::MarkdownContent::new("");
-    let form_box = cmdpal::content::form::FormContentBuilder::new()
+    let md_box = cmdpal::content::markdown::MarkdownContent::new("");
+    let form_box = cmdpal::content::form::FormContent::builder()
         .template_json(include_str!("./template.json"))
         .submit(|_, inputs, data| {
             tracing::info!("Form submitted with inputs: {}, data: {}", inputs, data);
             // Here you can process the form inputs and data
             // For now, just return KeepOpen to keep the form open
             Ok(CommandResult::KeepOpen)
-        })
-        .build();
+        });
     let task_box = md_box.clone();
     // start a thread to update the content to current time
-    let _handle = std::thread::spawn(move || unsafe {
+    let _handle = std::thread::spawn(move || {
         loop {
-            let time = Win32::System::SystemInformation::GetLocalTime();
+            let time = unsafe { windows::Win32::System::SystemInformation::GetLocalTime() };
             let time = format!(
                 "{}-{}-{} {:02}:{:02}:{:02}",
                 time.wYear, time.wMonth, time.wDay, time.wHour, time.wMinute, time.wSecond
@@ -74,100 +71,80 @@ fn com_main() -> Result<()> {
 - Model: {:?}
 "#,
                     time,
-                    token.lock().ok().map(|v| v.clone()).flatten(),
-                    temperature.lock().ok().map(|v| v.clone()).flatten(),
-                    toggle.lock().ok().map(|v| v.clone()).flatten(),
-                    model.lock().ok().map(|v| v.clone()).flatten()
+                    token.lock().ok().as_deref(),
+                    temperature.lock().ok(),
+                    toggle.lock().ok(),
+                    model.lock().ok(),
                 )
                 .into();
             }
             std::thread::sleep(std::time::Duration::from_secs(1));
         }
     });
-    let copy_sample_item: ComObject<CommandItem> = CommandItemBuilder::try_new(
-        cmdpal::cmd::common::copy_text::CopyTextCommandBuilder::new(
-            h!("This is a sample text to copy to clipboard").clone(),
-        )
-        .build()
-        .to_interface(),
-    )?
-    .title("Copy Sample Text")
-    .build();
+    let copy_sample_item = cmdpal::cmd::common::copy_text::CopyTextCommand::new(
+        h!("This is a sample text to copy to clipboard").clone(),
+    )
+    .item()?
+    .title("Copy Sample Text");
     let copy_time_box = md_box.clone();
-    let copy_time_item: ComObject<CommandItem> = CommandItemBuilder::try_new(
-        cmdpal::cmd::common::copy_text::CopyTextCommandBuilder::new_dyn(Box::new(move || {
+    let copy_time_item =
+        cmdpal::cmd::common::copy_text::CopyTextCommand::new_dyn(Box::new(move || {
             if let Ok(body) = copy_time_box.body() {
                 body.clone()
             } else {
                 "Failed to get current time".into()
             }
         }))
-        .build()
-        .to_interface(),
-    )?
-    .title("Copy Current Time Markdown")
-    .build();
-    let open_nonebot_dev_item = CommandItemBuilder::try_new(
-        cmdpal::cmd::common::open_url::OpenUrlCommandBuilder::new(
-            "https://nonebot.dev".to_string(),
-        )
-        .build()
-        .to_interface(),
-    )?
-    .title("Open nonebot.dev")
-    .build();
-    let reveal_file_item = CommandItemBuilder::try_new(
-        cmdpal::cmd::common::reveal_file::RevealFileCommandBuilder::new(
-            exe_path
-                .parent()
-                .unwrap()
-                .parent()
-                .unwrap()
-                .join("README.md"),
-        )
-        .build()
-        .to_interface(),
-    )?
-    .title("Reveal README.md in Explorer")
-    .build();
-    let cmd = cmdpal::page::content::ContentPageBuilder::new(
-        cmdpal::page::BasePageBuilder::new(
-            cmdpal::cmd::BaseCommandBuilder::new()
-                .name("Example Page")
-                .icon(IconInfo::new(IconData::from_glyph_and_family("M", "Wingdings"))) // M is a bomb in Wingdings
-                .id("BlueG.PEP.ExamplePage")
-                .build(),
-        )
+        .item()?
+        .title("Copy Current Time Markdown");
+    let open_nonebot_dev_item =
+        cmdpal::cmd::common::open_url::OpenUrlCommand::new("https://nonebot.dev")
+            .item()?
+            .title("Open nonebot.dev");
+    let reveal_file_item = cmdpal::cmd::common::reveal_file::RevealFileCommand::new(
+        exe_path
+            .parent()
+            .unwrap()
+            .parent()
+            .unwrap()
+            .join("README.md"),
+    )
+    .item()?
+    .title("Reveal README.md in Explorer");
+    let cmd = cmdpal::cmd::BaseCommand::builder()
+        .name("Example Page")
+        .icon(IconInfo::new(IconData::from_glyph_and_family(
+            "M",
+            "Wingdings",
+        ))) // M is a bomb in Wingdings
+        .id("BlueG.PEP.ExamplePage")
+        .page()
         .loading(false)
         .title("PEP Example Page")
-        .build(),
-    )
-    .details(
-        DetailsBuilder::new()
-            .title("Details Title")
-            .body("Details Body")
-            .build(),
-    )
-    .add_content(md_box)
-    .add_content(MarkdownContent::new(MD_CONTENT))
-    .add_content(form_box)
-    .add_context_item(CommandContextItemBuilder::new(copy_sample_item).build())
-    .add_context_item(CommandContextItemBuilder::new(copy_time_item).build())
-    .add_context_item(CommandContextItemBuilder::new(open_nonebot_dev_item).build())
-    .add_context_item(CommandContextItemBuilder::new(reveal_file_item).build())
-    .build();
-    let provider = CommandProviderBuilder::new()
+        .content()
+        .details(
+            Details::builder()
+                .title("Details Title")
+                .body("Details Body"),
+        )
+        .add_content(md_box)
+        .add_content(MarkdownContent::new(MD_CONTENT))
+        .add_content(form_box)
+        .add_context_item(copy_sample_item.context())
+        .add_context_item(copy_time_item.context())
+        .add_context_item(open_nonebot_dev_item.context())
+        .add_context_item(reveal_file_item.context());
+
+    let provider = CommandProvider::builder()
         .id("BlueG.PEP")
         .display_name("PEP Viewer")
         .icon(IconInfo::new(IconData::from(h!("\u{e8a5}").clone())))
         .frozen(true)
         .add_top_level(
-            CommandItemBuilder::try_new(cmd.to_interface())?
+            cmd.item()?
                 .icon(IconInfo::new(IconData::from(h!("\u{f6fa}").clone())))
                 .title("View PEP")
-                .subtitle("Open a PEP by number")
-                .build()
-                .to_interface(),
+                .subtitle("Open a PEP by number"),
         )
         .settings(settings.into())
         .build();
@@ -199,6 +176,7 @@ fn main() {
                 .with_line_number(true),
         )
         .init();
+    
     com_main().unwrap_or_else(|e| {
         tracing::error!("Error: {:?}", e);
         std::process::exit(1);
